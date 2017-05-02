b4ba5c66ec915514d480b97ff1b0c02d413e3c2b
longtime1116s <long.itomochi.time@gmail.com>
2017-05-02 20:35:58 +0900

    micropost 追加

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
-  # コントローラ名は application, アクション名は hello
-  root 'application#hello'
+  ## コントローラ名は application, アクション名は hello
+  #root 'application#hello'
+  # index アクションを使用するように変更
+  root 'users#index'
 end
